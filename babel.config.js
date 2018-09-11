7d7ae985ce9a07d3bb3d0f3cb952e2888de2df62
Aiden-Ziegelaar <aiden@thinkhub.io>
2018-09-11 13:21:34 +0800

    Added Veutify

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,22 @@
 module.exports = {
   presets: [
-    '@vue/app',
+    [
+      '@vue/app',
+      {
+        useBuiltIns: 'entry',
+      },
+    ],
+  ],
+  plugins: [
+    [
+      'transform-imports',
+      {
+        vuetify: {
+          // eslint-disable-next-line
+          transform: 'vuetify/es5/components/${member}',
+          preventFullImport: true,
+        },
+      },
+    ],
   ],
 };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container fluid fill-height="" class='background'>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld,
+  mounted() {
+    const self = this;
+    self.$nextTick(() => {
+
+    });
   },
 };
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.background{
+  background: linear-gradient(
+    135deg, rgba(70,159,236,1) 0%, rgba(93,195,239,1) 36%,
+    rgba(92,140,237,1) 68%, rgba(30,153,230,1) 100%);
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
