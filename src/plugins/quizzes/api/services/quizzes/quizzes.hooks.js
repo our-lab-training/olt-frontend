e2f1c5ac48fa461d02f971ef49506284ae7f7c99
frinzekt <frinzekt@gmail.com>
2020-08-07 15:53:29 +0800

    quizzes submodule

---
+++ src/plugins/quizzes/api/services/quizzes/quizzes.hooks.js
@@ -0,0 +1,35 @@
+
+const { authenticate } = require('@feathersjs/authentication').hooks;
+const safeRemove = require('../../../../../hooks/safe-remove');
+
+module.exports = {
+  before: {
+    all: [authenticate('jwt')],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: [safeRemove()]
+  },
+
+  after: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  },
+
+  error: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  }
+};
